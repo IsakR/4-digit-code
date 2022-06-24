20ba52f2d55aaef7f277991ec07bb690fa9fe0f6
Bjørn Isak Fauske <bjorn.isak@hotmail.com>
2022-06-24 06:54:26 +0200

    Update PyScript.py

--- PyScript.py
+++ PyScript.py
@@ -1,1 +1,15 @@
+#script by Fauske
+import random
+import math
+
+
+def rand_num(*args):
+    num = "145689876543"
+    four_digits = ""
+    for i in range(4):
+        four_digits = four_digits+ num[math.floor(random.random()*10)]
+    PyScript.write("thebuttonid", "New number", print(four_digits))
+
+
+rand_num()
 
