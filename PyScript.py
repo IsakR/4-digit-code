c9c34e321f780d6a7c257489a1fc02a93bc517c7
Bjørn Isak <bjorn.isak@hotmail.com>
2022-06-30 02:35:47 +0200

    Update PyScript.py

--- PyScript.py
+++ PyScript.py
@@ -1,12 +1,16 @@
 import random
 import math
 
+#generates the number
 def randomnum(*args):
     num = "145689876543"
     four_digits = ""
     for i in range(4):
         four_digits = four_digits+ num[math.floor(random.random()*10)]
+
+#priting the number on webpage 
     PyScript.write("thebuttonid", "New number")
     console.log(four_digits) 
     document.getElementById("PrintedNumber").innerHTML = four_digits
+
 randomnum()
